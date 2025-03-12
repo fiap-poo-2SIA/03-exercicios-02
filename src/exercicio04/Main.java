3acc0094374cf819d873e48209721e12e14ec32b
Laboratório FIAP <labsfiap@fiap.com.br>
2025-03-12 09:37:06 -0300

    enviando o exercício 04

---
+++ src/exercicio04/Main.java
@@ -0,0 +1,11 @@
+package exercicio04;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Funcionario f = new Funcionario("Ana", "Diretora", 80000);
+        System.out.println(f);
+        System.out.println(f.nome);
+
+    }
+}
