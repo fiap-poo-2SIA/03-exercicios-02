081712bfb3237173f075010ae105a9089fad3700
Laboratório FIAP <labsfiap@fiap.com.br>
2025-03-12 11:24:11 -0300

    atualização da classe Main do exercício 04

--- src/exercicio04/Main.java
+++ src/exercicio04/Main.java
@@ -1,11 +1,50 @@
 package exercicio04;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
 
-        Funcionario f = new Funcionario("Ana", "Diretora", 80000);
-        System.out.println(f);
-        System.out.println(f.nome);
+        Scanner sc = new Scanner(System.in);
+        int opcao;
+
+        do {
+            System.out.println("Escolha uma opção");
+            System.out.println("1. Cadastrar");
+            System.out.println("2. Pesquisar");
+            System.out.println("3. Listar");
+            System.out.println("4. Finalizar");
+            opcao = sc.nextInt();
+            if(opcao < 1 || opcao > 4) {
+                System.out.println("opção inválida");
+            }
+            else {
+                switch(opcao) {
+                    case 1:
+                        cadastrar();
+                        break;
+                    case 2:
+                        pesquisar();
+                        break;
+                    case 3:
+                        listar();
+                }
+            }
+
+            System.out.println();
+        } while(opcao != 4);
+
+    }
+
+    public static void cadastrar() {
+
+    }
+
+    public static void pesquisar() {
+
+    }
+
+    public static void listar() {
 
     }
 }
